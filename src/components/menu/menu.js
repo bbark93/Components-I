c1ec2548748c46f5bb90912d2a72a22a6177bb5e
Brandon Barksdale <bbark93@gmail.com>
2023-06-05 13:10:03 -0400

    first commit

--- src/components/menu/menu.js
+++ src/components/menu/menu.js
@@ -1,3 +1,4 @@
+import { Linter } from 'eslint';
 import './menu.less'
 
 // This is the data we will be using, study it but don't change anything, yet.
@@ -9,6 +10,31 @@ let menuItems = [
   'Music',
   'Log Out'
 ];
+
+function menuMaker(linksArr) {
+  const menuWrapper = document.createElement("div");
+  const menuList = document.createElement("ul");
+
+  menuWrapper.appendChild(menuList);
+
+  menuWrapper.classList.add('menu');
+
+  linksArr.array.forEach(linkText => {
+    const link = document.createElement("li");
+    link.textContent = linkText;
+    menuList.appendChild(link);
+  });
+
+  const hamMenu = document.querySelector('.menu-button');
+
+  hamMenu.addEventListener('click', () => {
+    menuWrapper.classList.toggle('menu--open');
+  });
+
+  return menuWrapper;
+}
+
+console.log(menuMaker(menuItems));
 
 /*
   Step 1: Write a component called 'menuMaker' to create a menu like the markup below:
